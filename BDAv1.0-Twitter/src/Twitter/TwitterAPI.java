36a13900decb92b16f60b0d09a9a0407e1516ca5
jmmpa111-iscteiul <jmmpa111@iscte-iul.pt>
2018-10-20 15:18:59 +0100

    Added Twitter project

---
+++ BDAv1.0-Twitter/src/Twitter/TwitterAPI.java
@@ -0,0 +1,64 @@
+package Twitter;
+
+import java.util.List;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class TwitterAPI {
+
+	public static List getTimeline(String user) throws TwitterException {
+		
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+    	cb.setDebugEnabled(true)
+    	  .setOAuthConsumerKey("W1f0VvgWPfT8OBqVxvy4Mw")
+    	  .setOAuthConsumerSecret("zKH2yAtRyefwsgOO8h8Szc4kru68iEm95QmIG7svw")
+    	  .setOAuthAccessToken("36481851-VhzByC4f9MSsZES1QZQ4e4iBvA9bWGLyv9HKFpy7c")
+    	  .setOAuthAccessTokenSecret("OahDuXF2Lhl5xlNYALhYZir6xSflAxKP9Zh89T05po");
+    	TwitterFactory tf = new TwitterFactory(cb.build());
+    	
+    	Twitter twitter = tf.getInstance();
+    	
+    	List<Status> statuses = twitter.getUserTimeline(user);
+    	
+    	return statuses;
+	}
+	
+	public static void printTimelineConsole(List<Status> statuses) {
+		System.out.println("------------------------\n Showing home timeline \n------------------------");
+ 		int counter=0;
+ 		int counterTotal = 0;
+        for (Status status : statuses) {
+				// Filters only tweets from user "catarina"
+				if (status.getUser().getName() != null && status.getUser().getName().contains("catarina")) {
+					System.out.println(status.getUser().getName() + ":" + status.getText());
+					counter++;
+				}
+				counterTotal++;
+         }
+ 		System.out.println("-------------\nNº of Results: " + counter+"/"+counterTotal);
+	}
+	
+	public static String getTimelineText(List<Status> statuses) {
+		String result = "";
+		result += "------------------------\n Showing user's timeline \n------------------------" + "\n";
+ 		int counter=1;
+        for (Status status : statuses) {
+        	result += "@"+ status.getUser().getName() + ": " + status.getText() + "\n" +
+        			  "Retweets: " + status.getRetweetCount() + "         Favorites: " + status.getFavoriteCount() + "\n" +
+        			  "---------------Tweet Nº " + counter + "---------------\n";
+        	counter++;
+        }
+        return result;
+	}
+	
+	public static String getTweet(Status status) {
+		String result = "";
+		result += "@"+ status.getUser().getName() + ": " + status.getText() + "\n" +
+  			  "Retweets: " + status.getRetweetCount() + "         Favorites: " + status.getFavoriteCount();
+		return result;
+	}
+}
---
+++ BDAv1.0-Twitter/src/Twitter/TwitterMain.java
@@ -0,0 +1,17 @@
+package Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+import java.util.List;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+
+public class TwitterMain {
+
+	public static void main(String[] args) {
+		
+    	
+	}
+
+}
