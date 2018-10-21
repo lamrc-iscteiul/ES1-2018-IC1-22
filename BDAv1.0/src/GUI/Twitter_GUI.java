f5b7e1448495f481f4dcdbed695f02479718dd21
aadau-iscteiul <aadau@iscte-iul.pt>
2018-10-21 01:13:24 +0100

    Major update Twitter

--- BDAv1.0/src/GUI/Twitter_GUI.java
+++ BDAv1.0/src/GUI/Twitter_GUI.java
@@ -3,14 +3,25 @@ import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+
+import Twitter.TwitterAPI;
+import twitter4j.Status;
+import twitter4j.TwitterException;
 
 public class Twitter_GUI  {
 	private JPanel panel;
+	private JTextArea feed;
+	private JFrame frame;
+	
 	//private Component[] informacaoTweet;
 	
 	public Twitter_GUI() {
@@ -36,6 +47,29 @@ public class Twitter_GUI  {
 		
 		Button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				List<Status> status_iscte = null;
+				
+				feed = new JTextArea ( 80,2);
+				JScrollPane scrollArea = new JScrollPane(feed);
+				try {
+					status_iscte = TwitterAPI.getTimeline("ISCTEIUL");
+				} catch (TwitterException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					List<Status> status_biblio = TwitterAPI.getTimeline("BibliotecaISCTE");
+				} catch (TwitterException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				frame = new JFrame();
+				frame.setBounds(0, 0, 800,735);
+				feed.append(TwitterAPI.getTimelineText(status_iscte));
+				JScrollPane scroll = new JScrollPane(feed);
+				frame.add(scroll);
+				frame.setVisible(true);
 			}
 		});
 		}
