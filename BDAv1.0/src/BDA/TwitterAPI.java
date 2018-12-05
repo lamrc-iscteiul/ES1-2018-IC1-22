61ff82b67820c402c25ca9d5aff17ae428a123ba
jmmpa111-iscteiul <jmmpa111@iscte-iul.pt>
2018-12-05 23:11:25 +0000

    Added Retweet, Favorite and Reply functionality to TwitterAPI

--- BDAv1.0/src/BDA/TwitterAPI.java
+++ BDAv1.0/src/BDA/TwitterAPI.java
@@ -11,6 +11,7 @@ import javax.xml.bind.Unmarshaller;
 
 import twitter4j.Paging;
 import twitter4j.Status;
+import twitter4j.StatusUpdate;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -18,14 +19,48 @@ import twitter4j.conf.ConfigurationBuilder;
 
 public class TwitterAPI {
 	
-	private final static int TWEET_NUMBER = 100;
+	private int tweet_number = 100;
+	private Twitter twitter;
+	
+	public TwitterAPI() {
+		
+		configXML configXML = null;
+		//lê ficheiro XML
+		try {
+			File file = new File("config.xml");
+	        JAXBContext jaxbContext = JAXBContext.newInstance(configXML.class);
+	        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+	        configXML = (configXML) unmarshaller.unmarshal(file);
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+		
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+    	cb.setDebugEnabled(true)
+    	  .setOAuthConsumerKey(configXML.getTwitter().getConsumerKey())
+    	  .setOAuthConsumerSecret(configXML.getTwitter().getConsumerKeySecret())
+    	  .setOAuthAccessToken(configXML.getTwitter().getAccessToken())
+    	  .setOAuthAccessTokenSecret(configXML.getTwitter().getAccessTokenSecret());
+    	TwitterFactory tf = new TwitterFactory(cb.build());
+    	
+    	twitter = tf.getInstance();
+	}
+	
+	public void setTweetNumber(int x) {
+		this.tweet_number = x;
+	}
+	
+	public int getTweetNumber() {
+		return tweet_number;
+	}
 	
 	/**Gets the List containing objects of the type GeneralMessage
 	 * 
 	 * @param user
 	 * @return
 	 */
-	public static ArrayList<GeneralMessage> getList(String user) {
+	
+	public ArrayList<GeneralMessage> getList(String user) {
 		ArrayList<GeneralMessage> list = new ArrayList<GeneralMessage>();
 		try {
 			List<Status> statuses = getTimeline(user);
@@ -45,31 +80,33 @@ public class TwitterAPI {
 	 * @param user Desired user's usernam
 	 * @returns List of the user's statuses
 	 */
-	public static List<Status> getTimeline(String user) throws TwitterException {
+	public List<Status> getTimeline(String user) throws TwitterException {
 		
-		configXML configXML = null;
-		//lê ficheiro XML
-		try {
-			File file = new File("config.xml");
-	        JAXBContext jaxbContext = JAXBContext.newInstance(configXML.class);
-	        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-	        configXML = (configXML) unmarshaller.unmarshal(file);
-		} catch (JAXBException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		configXML configXML = null;
+//		//lê ficheiro XML
+//		try {
+//			File file = new File("config.xml");
+//	        JAXBContext jaxbContext = JAXBContext.newInstance(configXML.class);
+//	        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+//	        configXML = (configXML) unmarshaller.unmarshal(file);
+//		} catch (JAXBException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		Paging page = new Paging(1,TWEET_NUMBER);
+//		
+//		ConfigurationBuilder cb = new ConfigurationBuilder();
+//    	cb.setDebugEnabled(true)
+//    	  .setOAuthConsumerKey(configXML.getTwitter().getConsumerKey())
+//    	  .setOAuthConsumerSecret(configXML.getTwitter().getConsumerKeySecret())
+//    	  .setOAuthAccessToken(configXML.getTwitter().getAccessToken())
+//    	  .setOAuthAccessTokenSecret(configXML.getTwitter().getAccessTokenSecret());
+//    	TwitterFactory tf = new TwitterFactory(cb.build());
+//    	
+//    	Twitter twitter = tf.getInstance();
 		
-		Paging page = new Paging(1,TWEET_NUMBER);
-		
-		ConfigurationBuilder cb = new ConfigurationBuilder();
-    	cb.setDebugEnabled(true)
-    	  .setOAuthConsumerKey(configXML.getTwitter().getConsumerKey())
-    	  .setOAuthConsumerSecret(configXML.getTwitter().getConsumerKeySecret())
-    	  .setOAuthAccessToken(configXML.getTwitter().getAccessToken())
-    	  .setOAuthAccessTokenSecret(configXML.getTwitter().getAccessTokenSecret());
-    	TwitterFactory tf = new TwitterFactory(cb.build());
-    	
-    	Twitter twitter = tf.getInstance();
+		Paging page = new Paging(1,tweet_number);
     	
     	List<Status> statuses = twitter.getUserTimeline(user, page);
     	
@@ -122,5 +159,35 @@ public class TwitterAPI {
 		result += "@"+ status.getUser().getName() + ": " + status.getText() + "\n" +
   			  "Retweets: " + status.getRetweetCount() + "         Favorites: " + status.getFavoriteCount();
 		return result;
+	}
+	
+	public void reply(Status status, String reply_text) {
+		try {
+			long reply_id = status.getInReplyToStatusId();
+			
+			StatusUpdate reply = new StatusUpdate(reply_text);
+			reply.setInReplyToStatusId(reply_id);
+			twitter.updateStatus(reply);
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void favorite(Status status) {
+		try {
+			long tweet_id = status.getId();
+			twitter.createFavorite(tweet_id);
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void retweet(Status status) {
+		try {
+			long tweet_id = status.getId();
+			twitter.retweetStatus(tweet_id);
+		}catch (TwitterException e) {
+			e.printStackTrace();
+		}
 	}
 }
